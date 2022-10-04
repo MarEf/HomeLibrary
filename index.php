59f331955d1e283a2f4412e8dd4ff106cf8c8921
Maria <maria.efimova93@gmail.com>
2022-10-04 15:08:53 +0300

    API lookup functional

--- index.php
+++ index.php
@@ -1,31 +1,114 @@
 <?php
-$isbn_pattern = "^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-\s]+$";
-$api_url = "https://openlibrary.org/isbn/";
+$isbn_pattern = "^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d\-\s]+$|(?:\D*\d){9}[xX]$";
+$api_url = "https://openlibrary.org/api/books?bibkeys=ISBN:";
+$data_format = "&jscmd=data&format=json";
+
+function fetchTitle($book)
+{
+    $title = "";
+    if (@$book['title']) {
+        $title = $book['title'];
+    }
+    return $title;
+}
+
+function fetchAuthors($book)
+{
+    $authors = [];
+    if (@$book['authors']) {
+        $author_array = $book['authors'];
+        foreach ($author_array as $key => $author) {
+            array_push($authors, $author_array[$key]['name']);
+        }
+    }
+    return implode(", ", $authors);
+}
+
+function fetchCover($book)
+{
+    $cover = "";
+    if (@$book['cover']) {
+        $cover = end($book['cover']);
+    }
+    return $cover;
+}
+
+function fetchISBN($book, $isbn_nro)
+{
+    $isbn = "";
+    if (@$book['identifiers']['isbn_' . $isbn_nro]) {
+        $isbn = $book['identifiers']['isbn_' . $isbn_nro][0];
+    }
+    return $isbn;
+}
 
 if (isset($_POST["search"])) {
     /*
     Book information we are interested in:
         * Title
         * Author(s)
-        * Cover ("https://covers.openlibrary.org/b/isbn/". $isbn ."-L.jpg" />)
+        * Cover
         * ISBN-10
         * ISBN-13
+
+    Test cases:
+        * All data available: 9789022543207
+        * Missing ISBN-10: 9780316270366
+        * Multiple authors: 9780316304788
+        * Missing cover and author: 9784048936941
+        * Missing author: 9784048663168
     */
+
+    # Initialize values;
+    $title = "";
+    $authors = "";
+    $cover = "";
+    $isbn10 = "";
+    $isbn13 = "";
+    $source = "none";
 
     # Remove non-numbers from search
     $isbn = preg_replace("/\D|_/", '', $_POST["isbn"]);
     # Check local database for existing entry
 
+
+
     # If fail, search OpenLibrary
-    $url = $api_url . $isbn . ".json";
-    $book = @file_get_contents($url); #yök
+    $url = $api_url . $isbn . $data_format;
 
-    if ($book !== false) {
+    # ALWAYS returns a JSON-string! On failure, the string is {}.
+    $book = file_get_contents($url);
+
+    # If a book exists on OpenLibrary, fetch its data.
+    if ($book !== '{}') {
         # Handle book data
+        # Convert JSON string to associative array
+        $book = json_decode($book, true);
+        $book = $book['ISBN:' . $isbn];
 
-    } else {
-        #Display error: "No book matching the given ISBN was found. Please check your input or add the book to your collection manually.";
+        # Fetch the data you're interested in
+        $title = fetchTitle($book);
+        $authors = fetchAuthors($book);
+        $cover = fetchCover($book);
+        $isbn10 = fetchISBN($book, 10);
+        $isbn13 = fetchISBN($book, 13);
+        $source = "OpenLibrary";
     }
+
+    # Send book data to book page.
+    echo "
+        <form id='book_data' methor='POST' action='book.php'>
+            <input type='hidden' name='title' value='$title'>
+            <input type='hidden' name='authors' value='$authors'>¨
+            <input type='hidden' name='cover' value='$cover'>
+            <input type='hidden' name='isbn10' value='$isbn10'>
+            <input type='hidden' name='isbn13' value='$isbn13'>
+            <input type='hidden' name='source' value='$source'>
+        </form>
+        <script>
+            querySelector('#book_data').submit();
+        </script>
+    ";
 }
 ?>
 
